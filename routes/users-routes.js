3eba164df3280590ddcda6518fe3018383cb8232
petrikur <patekuronen123@gmail.com>
2023-05-10 21:34:02 +0300

    fix filenames

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const usersController = require("../controllers/users-controller")
 const router = express.Router();
+const usersController = require("../controllers/users-controller")
 const auth = require("../auth/auth");
 const rateLimiter = require('../middleware/rateLimiter');
 
